7faf607831e2eb1f37c00fc9c99e0ab37ff72853
MAITRIYA SRAMANER <maitriya@outlook.fr>
2025-01-30 16:06:25 +0100

    resolved game problems

--- App.js
+++ App.js
@@ -1,11 +1,11 @@
 import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState, useEffect } from 'react';
-import { ImageBackground } from 'react-native-web';
 
 
 // MAX_TIME : durée en secondes pour répondre
 // MAX_NUMBER : limite supérieure pour la génération des nombres aléatoires
 const MAX_TIME = 30;
+const HARD_MODE_TIME = 15;
 const MAX_NUMBER = 50;
 
 
@@ -50,7 +50,7 @@ export default function App() {
             setFirstNumber(RandomNumber());
             setSecondNumber(RandomNumber());
             setThirdNumber(RandomNumber());
-            setTime(gameMode === "hard" ? 10 : 30);
+            setTime(gameMode === "hard" ? HARD_MODE_TIME : MAX_TIME);
             setUserAnswer("");
         }
         else {
@@ -67,17 +67,17 @@ export default function App() {
         setFirstNumber(RandomNumber());
         setSecondNumber(RandomNumber());
         setThirdNumber(RandomNumber());
-        setTime(gameMode === "hard" ? 10 : 30);
+        setTime(gameMode === "hard" ? HARD_MODE_TIME : MAX_TIME);
         setUserAnswer(0);
         setbtnEnable(true);
         setMessage("");
     }
 
 
-    // use effect qui permet de faire le calcule en fonction du nombre généré
+    // use effect qui permet de faire le calcule en fonction du nombre généré + fonction ternaire pour hard mode
     useEffect(() => {
-        setSolution(() => firstNumber + secondNumber + thirdNumber)
-    }, [firstNumber, secondNumber, thirdNumber])
+        setSolution(() => firstNumber + secondNumber + (gameMode === "hard" ? thirdNumber : 0))
+    }, [firstNumber, secondNumber, thirdNumber, gameMode])
 
 
     // use effect qui permet de faire le timer
@@ -97,8 +97,8 @@ export default function App() {
 
     return (
         <View style={styles.body}>
-            
-             {gameMode == 'menu' ? null : <Text style={styles.timer}>{formatTime(time)}</Text>}
+
+            {gameMode == 'menu' ? null : <Text style={styles.timer}>{formatTime(time)}</Text>}
 
             <View style={styles.container}>
 
@@ -108,16 +108,16 @@ export default function App() {
                     <View style={styles.box}>
                         <Text style={styles.title}>Math Game</Text>
                         <View style={styles.menubutton}>
-                        <Button
-                            title="EASY"
-                            style={styles.button}
-                            onPress={() => {setGameMode('easy'); setTime(MAX_TIME);}}
-                        />
-                        <Button
-                            title="HARD"
-                            style={styles.button}
-                            onPress={() => { setGameMode('hard'); setTime(10); }}
-                        />
+                            <Button
+                                title="EASY"
+                                style={styles.button}
+                                onPress={() => { setGameMode('easy'); setTime(MAX_TIME); }}
+                            />
+                            <Button
+                                title="HARD"
+                                style={styles.button}
+                                onPress={() => { setGameMode('hard'); setTime(HARD_MODE_TIME); }}
+                            />
                         </View>
                     </View>
                 ) : (
@@ -125,9 +125,9 @@ export default function App() {
                     <View style={styles.box}>
                         {/* Si gameMode est 'menu', affiche le menu, sinon affiche le jeu */}
 
-                       
+
                         {/* Affiche le troisième nombre uniquement en mode 'hard' */}
-                        <Text style={styles.number}>{firstNumber} + {secondNumber} {gameMode === 'hard' ? "+ " + thirdNumber : ''}= ?</Text>
+                        <Text style={styles.number}>{firstNumber} + {secondNumber} {gameMode === 'hard' ? "+ " + thirdNumber : ''} = ?</Text>
                         <TextInput style={styles.answer}
                             placeholder='answer'
                             keyboardType="numeric"
@@ -136,32 +136,34 @@ export default function App() {
                         />
                         <View style={styles.submit}>
 
-                        <Button color="green"
-                            
-                        
-                            title="Submit"
-                            onPress={handleSubmit}
-                            disabled={!btnEnable}
-                        />
+                            <Button color="green"
+
+
+                                title="Submit"
+                                onPress={handleSubmit}
+                                disabled={!btnEnable}
+                            />
                         </View>
-                        
-                        <Text style= {[
+
+                        <Text style={[
                             styles.message,
 
-                            {color: message === 'Right answer !' ? 'green'
-                                : message === 'Wrong answer!' ? 'red'
-                                    : 'black'
-                        }]}>
+                            {
+                                color: message === 'Right answer !' ? 'green'
+                                    : message === 'Wrong answer!' ? 'red'
+                                        : 'black'
+                            }]}>
                             {message}
                         </Text>
-                        <Button  color="red" style={styles.reset} title='Reset' onPress={handleReset} />
+                        <Button color="red" style={styles.reset} title='Reset' onPress={handleReset} />
 
                     </View>
                 )}
             </View>
+              {/* Affiche le new game  uniquement en mode 'easy/hard'  et appel la fonction reset*/}
             <View style={styles.newGame}>
-                {gameMode === 'menu' ? null : <Button color="black" title='New Game' onPress={() => setGameMode('menu')}></Button>}
-               
+                {gameMode === 'menu' ? null : <Button color="black" title='New Game' onPress={() => {handleReset(), setGameMode('menu')}}></Button>}
+
             </View>
         </View>
     );
@@ -176,21 +178,21 @@ const styles = StyleSheet.create({
     },
 
     timer: {
-        fontSize: 48, 
+        fontSize: 48,
         fontWeight: 'bold',
-        color: '#333', 
+        color: '#333',
         textAlign: 'center',
         padding: 10,
         backgroundColor: 'white',
-        borderRadius: 10, 
+        borderRadius: 10,
         borderWidth: 2,
         borderColor: '#333',
         marginBottom: 20,
-        width: 150,  
-        alignSelf: 'center',  
+        width: 150,
+        alignSelf: 'center',
     },
     container: {
-        
+
     },
     box: {
         padding: 20,
@@ -200,53 +202,45 @@ const styles = StyleSheet.create({
         alignItems: 'center',
     },
     number: {
-        fontSize: 50,
+        fontSize: 45,
 
     },
-  
-answer: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 10,
-    borderRadius: 10,
-    borderWidth: 2,
-    borderColor: '#333',
-    marginTop: 60,  
-    width: 200,  
-},
-message: {
-    fontSize: 20,
-    marginTop: 40,
-    marginBottom: 40,
-},
-submit : {
-    marginTop: 50,
-    width: 200,
-},
+
+    answer: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#333',
+        marginTop: 60,
+        width: 200,
+    },
+    message: {
+        fontSize: 20,
+        marginTop: 40,
+        marginBottom: 40,
+    },
+    submit: {
+        marginTop: 50,
+        width: 200,
+    },
 
     newGame: {
         backgroundColor: 'black',
         width: 200,
-        alignSelf: 'center',  
+        alignSelf: 'center',
         marginTop: 100,
 
     },
 
     menubutton: {
-        flexDirection: 'row', 
-        gap: 20,             
-        justifyContent: 'center', 
+        flexDirection: 'row',
+        gap: 20,
+        justifyContent: 'center',
         width: '100%',
         marginTop: 100,
-    },
-
-    button: {
-        marginTop: 20,
-        backgroundColor: 'pink',
-        
-    
-
     },
     title: {
         fontSize: 40,
