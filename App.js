1acae60c66263b8f6fca103672cd8d2e508fc8fa
MAITRIYA SRAMANER <maitriya@outlook.fr>
2025-01-30 17:16:04 +0100

    added score for fun

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 import { useState, useEffect } from 'react';
 
 
-// MAX_TIME : durée en secondes pour répondre
+// MAX_TIME : durée en secondes pour répondre en easy
+// HARD_MODE_TIME : durée en secondes pour répondre en hard
 // MAX_NUMBER : limite supérieure pour la génération des nombres aléatoires
 const MAX_TIME = 30;
 const HARD_MODE_TIME = 15;
@@ -30,11 +31,12 @@ export default function App() {
     const [secondNumber, setSecondNumber] = useState(RandomNumber());
     const [thirdNumber, setThirdNumber] = useState(RandomNumber());
     const [solution, setSolution] = useState(0);
-    const [userAnswer, setUserAnswer] = useState(0);
+    const [userAnswer, setUserAnswer] = useState("");
     const [message, setMessage] = useState("");
     const [time, setTime] = useState(MAX_TIME);
     const [btnEnable, setbtnEnable] = useState(true);
     const [gameMode, setGameMode] = useState('menu');
+    const [score, setScore] = useState(0);
 
 
     // fonction pour réduire le temps
@@ -50,11 +52,15 @@ export default function App() {
             setFirstNumber(RandomNumber());
             setSecondNumber(RandomNumber());
             setThirdNumber(RandomNumber());
+            setScore(prevscore => prevscore + 10);
             setTime(gameMode === "hard" ? HARD_MODE_TIME : MAX_TIME);
             setUserAnswer("");
         }
         else {
             setMessage('Wrong answer!')
+            setScore(prevscore => prevscore - 3);
+            setUserAnswer("");
+
         }
     }
 
@@ -62,7 +68,7 @@ export default function App() {
     // Fonction de réinitialisation du jeu :
     // - Génère de nouveaux nombres aléatoires
     // - Réinitialise le timer au temps maximum
-    // - Réinitialise l'interface (réponse utilisateur, bouton, messages)
+    // - Réinitialise l'interface (réponse utilisateur, bouton, messages,score)
     const handleReset = () => {
         setFirstNumber(RandomNumber());
         setSecondNumber(RandomNumber());
@@ -71,6 +77,7 @@ export default function App() {
         setUserAnswer(0);
         setbtnEnable(true);
         setMessage("");
+        setScore(0);
     }
 
 
@@ -87,26 +94,33 @@ export default function App() {
     }, [])
 
 
-    // useffect qui enleve le bouton submit et envoie un message de fin de partie avec la réponse attendu lorsque le temps atteint 0
+    // useffect qui enleve le bouton submit et envoie un message de fin de partie avec la solution attendu lorsque le temps atteint 0
     useEffect(() => {
         if (time === 0 && gameMode !== 'menu') {
             setbtnEnable(false);
-            setMessage("Time's up ! The right answer was " + solution)
+            setMessage("Time's up ! The correct answer was " + solution + " \n\nPress RESET to try again or NEW GAME to change the difficulty.")
         }
     }, [time])
 
+
+
     return (
+
         <View style={styles.body}>
+            {/* Section qui affiche le menu du jeu*/}
+
 
             {gameMode == 'menu' ? null : <Text style={styles.timer}>{formatTime(time)}</Text>}
 
             <View style={styles.container}>
 
-                {/* Section qui affiche le menu du jeu , soit en easy soit hard */}
+                {/* Si gameMode est 'menu', affiche le menu, sinon affiche le jeu */}
 
                 {gameMode === 'menu' ? (
                     <View style={styles.box}>
                         <Text style={styles.title}>Math Game</Text>
+                        <Text style={styles.scoreContainer}>Choose difficulty level</Text>
+
                         <View style={styles.menubutton}>
                             <Button
                                 title="EASY"
@@ -115,6 +129,7 @@ export default function App() {
                             />
                             <Button
                                 title="HARD"
+                                color="darkred"
                                 style={styles.button}
                                 onPress={() => { setGameMode('hard'); setTime(HARD_MODE_TIME); }}
                             />
@@ -123,7 +138,6 @@ export default function App() {
                 ) : (
 
                     <View style={styles.box}>
-                        {/* Si gameMode est 'menu', affiche le menu, sinon affiche le jeu */}
 
 
                         {/* Affiche le troisième nombre uniquement en mode 'hard' */}
@@ -155,26 +169,32 @@ export default function App() {
                             }]}>
                             {message}
                         </Text>
+                        <Text style={styles.scoreContainer}>
+                            Score: <Text style={styles.scoreNumber}>{score}</Text>
+                        </Text>
                         <Button color="red" style={styles.reset} title='Reset' onPress={handleReset} />
 
                     </View>
                 )}
             </View>
-              {/* Affiche le new game  uniquement en mode 'easy/hard'  et appel la fonction reset*/}
+            {/* Affiche le bouton new game uniquement en mode 'easy/hard' et appel la fonction reset*/}
             <View style={styles.newGame}>
-                {gameMode === 'menu' ? null : <Button color="black" title='New Game' onPress={() => {handleReset(), setGameMode('menu')}}></Button>}
+                {gameMode === 'menu' ? null : <Button color="black" title='New Game' onPress={() => { handleReset(), setGameMode('menu') }}></Button>}
 
             </View>
         </View>
     );
 }
 
+
+// Styles 
+
 const styles = StyleSheet.create({
     body: {
 
         flex: 1,
         justifyContent: 'center',
-        backgroundColor: 'lightblue',
+        backgroundColor: '#B3D9FF',
     },
 
     timer: {
@@ -187,17 +207,30 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 2,
         borderColor: '#333',
+        marginTop: 50,
         marginBottom: 20,
         width: 150,
         alignSelf: 'center',
     },
-    container: {
-
+    scoreContainer: {
+        backgroundColor: '#333',
+        padding: 10,
+        borderRadius: 15,
+        marginBottom: 20,
+        alignSelf: 'center',
+        minWidth: 150,
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },
+    scoreNumber: {
+        color: '#FFD700',
+        fontSize: 24,
+        fontWeight: 'bold',
     },
     box: {
         padding: 20,
-        borderWidth: 2,
-        borderColor: 'black',
         backgroundColor: 'white',
         alignItems: 'center',
     },
@@ -214,7 +247,7 @@ const styles = StyleSheet.create({
         borderRadius: 10,
         borderWidth: 2,
         borderColor: '#333',
-        marginTop: 60,
+        marginTop: 20,
         width: 200,
     },
     message: {
@@ -224,14 +257,15 @@ const styles = StyleSheet.create({
     },
     submit: {
         marginTop: 50,
-        width: 200,
+        width: 100,
     },
 
     newGame: {
         backgroundColor: 'black',
         width: 200,
         alignSelf: 'center',
-        marginTop: 100,
+        marginTop: 50,
+        marginBottom: 50,
 
     },
 
@@ -239,11 +273,10 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         gap: 20,
         justifyContent: 'center',
-        width: '100%',
-        marginTop: 100,
+        marginTop: 30,
     },
     title: {
-        fontSize: 40,
+        fontSize: 60,
         fontWeight: 'bold',
         marginBottom: 20,
     }
